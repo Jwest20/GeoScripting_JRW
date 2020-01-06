db4f7aeebbe660d57032d31866a9f895eebfeb61
Westdijk, Jos <jos.westdijk@gmail.com>
2020-01-06 14:49:17 +0100

    Updating Files Adding Main R File

---
+++ main.R
@@ -0,0 +1,18 @@
+# Team Teamname (Jos Westdijk and) 
+# January 2020
+
+## IMPORTANT: Set your working Directory
+setwd("D:/Documents/D/Universiteit/Jaar_6/GeoScripting/GeoScripting_JRW")
+
+# Import packages
+library(raster)
+library(sf)
+
+# Source functions
+source('R/function1.R')
+source('R/function1.R')
+
+# Load datasets
+load('data/input_model.rda')
+
+# commands
